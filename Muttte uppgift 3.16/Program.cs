85e2379202de99d0e5e1d2e62c80864a1454a506
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-06 13:54:12 +0200

    Add project files.

---
+++ Muttte uppgift 3.16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_3._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur lång är din låt? ");
+            int time = int.Parse(Console.ReadLine());
+            int mi = int.Parse(Console.ReadLine());
+            int total = int.Parse(Console.ReadLine());
+            int t = 260;
+            int m = 165;
+
+            if (total <= t && total >= m)
+            {
+                Console.WriteLine("din låt får vara med !");
+            }
+
+            else { Console.WriteLine("tyvär får inte din låt vara med"); }
+
+        }
+    }
+}
